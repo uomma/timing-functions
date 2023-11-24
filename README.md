85721ac4bd47505f6031f25766729040ef7540b5
uomma <marcelloleone975@gmail.com>
2023-11-24 11:55:41 +0100

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+visualizzare in pagina 5 numeri casuali
+
+da li parte un timer di 30 sec
+
+dopo 30sec i numeri scompaiono
+
+e utente deve inserire uno allavolta i numeri visualizzati
+
+successivamente i l software identifica e dice quali numeri sono stati inseriti correttamente
--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
 'use strict'
 
-console.log('ciao')
+
+/* function hello(){
+   alert('ti sto salutando') 
+} */
+//setTimeout(hello, 4000);
+/* 
+
+function bo() {
+    console.log('non lo so')
+}
+const timer = setInterval(bo, 2000);
+console.log('timer');
+
+const stop = document.getElementById('stop');
+stop.addEventListener('click', function () {
+    console.log('stoppo il timer');
+    //per fermare il timer uso CLEARINTERVAL
+    clearInterval(timer)
+});
+ */
+
+
+//chiedi al utente quanti secondi mancano alla cottura della pasta
+
+/* const seconds = Number(prompt('quanto manca?'))
+console.log({seconds})
+
+setTimeout(function(){
+alert('è pronta')
+}, seconds*1000); */
+
+//COUNTDOWN OTTIMIZZATO
+let tempo = 10;
+
+const clock = setInterval(function(){
+       
+    if(tempo === 0){
+        clearInterval(clock);
+        console.log('buon anno!!')
+    }else{
+        console.log(tempo);
+        tempo--;
+    }
+},  1000);
